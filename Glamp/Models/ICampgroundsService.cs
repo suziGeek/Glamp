6209259b7f1f13034372c0282879e38093cea7ca
Suzanne <46833370+suziGeek@users.noreply.github.com>
2020-09-08 09:03:55 -0400

    stuff

---
+++ Glamp/Models/ICampgroundsService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Glamp.Models
+{
+    interface ICampgroundsService
+    {
+      
+            Task<List<Campgrounds>> GetPaginatedResult(int currentPage, int pageSize = 10);
+            Task<int> GetCount();
+        
+    }
+}
